c8cc7ba68cd77e50950f30676c7ec90746b1579a
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-04-23 18:44:42 +0000

    yur

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kenneth
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function randomCheck() {
+  const userNumber = parseInt(document.getElementById("num-entered").value)
+
+  if (userNumber == randomNumber) {
+    document.getElementById("answer").innerHTML = "Correct"
+  }
+  if (userNumber != randomNumber) {
+    document.getElementById("answer").innerHTML = "Incorrect, try again"
+  }
+  // Output the answer to the user in the class "answer"
+}
